eb7a4c1f1f0c62d941571fea0a1813bdde8a6d97
Kyle Young <53121518+kyleyoung1121@users.noreply.github.com>
2023-02-26 09:22:28 -0500

    Update main_no_ui.py

--- main_no_ui.py
+++ main_no_ui.py
@@ -138,7 +138,7 @@ def run():
 def deminish_queue():
     global queue_length, sp
     while True:
-        time.sleep(10)
+        time.sleep(250)
         if queue_length > 0:
             queue_length -= 1
 
